c338d3f534440e4a5b8593d1cb74f61cc55646f8
Nida0709 <rikutocbr0709@outlook.com>
2023-01-09 18:09:30 +0900

    ver1.0.1

--- WeibullDistribution.py
+++ WeibullDistribution.py
@@ -1,6 +1,7 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import math
 df = pd.DataFrame({'k-cycles':
                   [70, 90, 96, 97, 99, 100, 103, 104, 104, 105,
                     107, 108, 108, 108, 109, 109, 112, 112, 113, 
@@ -14,21 +15,50 @@ df = pd.DataFrame({'k-cycles':
                     155, 156, 157, 157, 157, 157, 158, 159, 162,
                     163, 163, 164, 166, 166, 168, 170, 174, 196, 212]})
 #summary df makeing
-Vertical_axis = []
+Vertical_axis1 = []
+Vertical_axis2 = []
 Horizontal_axis = []
 for i in range(0, 240, 10):
+    Vertical_axis1.append(0)        #Vertical axis1
+    Vertical_axis2.append(0)        #Vertical axis2
     Horizontal_axis.append(i)      #Horizontal axis
-    Vertical_axis.append(0)        #Vertical axis
 
 
 for row in df['k-cycles']:
     for i in range(0, 240, 10):
         if row>=i and row<i+10:
-            Vertical_axis[int(i/10)] += 1
-
+            Vertical_axis1[int(i/10)] += 1
+for i in range(0, 240, 10):
+    if i == 0:
+        Vertical_axis2[int(i/10)] = Vertical_axis1[int(i/10)]
+    else:
+        Vertical_axis2[int(i/10)] = Vertical_axis2[int((i-10)/10)] + Vertical_axis1[int(i/10)]
 
 #transform from list to ndarray
-np_Vertical_axis = np.array(Vertical_axis)
+np_Vertical_axis1 = np.array(Vertical_axis1)
+np_Vertical_axis2 = np.array(Vertical_axis2)
 np_Horizontal_axis = np.array(Horizontal_axis)
-plt.bar(np_Horizontal_axis, np_Vertical_axis, width=10)
+
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(2, 2, 1)
+ax2 = fig1.add_subplot(2, 2, 2)
+ax3 = fig1.add_subplot(2, 2, 3)
+ax4 = fig1.add_subplot(2, 2, 4)
+
+
+ax1.set_title('Number of failures')
+ax1.set_xlabel('Repeated cycle number (10^3)')
+ax1.set_ylabel('Number of failures')
+ax1.bar(np_Horizontal_axis, np_Vertical_axis1, width=10)
+
+ax2.set_title('Total number of failures')
+ax2.set_xlabel('Repeated of Number (10^3)')
+ax2.set_ylabel('Total number of failures')
+ax2.bar(np_Horizontal_axis, np_Vertical_axis2, width=10)
+
+ax3.set_title('Probabilty densify function')
+ax3.plot(np_Horizontal_axis, np_Vertical_axis1)
+
+ax4.plot(np.log(np_Horizontal_axis), np.log(np.log(1/(1-np_Vertical_axis1))))
+
 plt.show()
